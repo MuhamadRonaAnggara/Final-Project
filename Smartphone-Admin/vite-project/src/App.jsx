2ef7f66957dd8387efb1c0ed3f9cc9d81aa67f4c
Wahyu Aji <wajinusantara05@gmail.com>
2024-07-21 22:55:22 +0700

    Log

--- Smartphone-Admin/vite-project/src/App.jsx
+++ Smartphone-Admin/vite-project/src/App.jsx
@@ -1,30 +1,49 @@
-import {useState} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// App.jsx
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Home from './Home';
 import Product from './Product';
+import Login from './Login';
 
 function App() {
-  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const authStatus = localStorage.getItem('isAuthenticated');
+    if (authStatus) {
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   const OpenSidebar = () => {
-    setOpenSidebarToggle(!openSidebarToggle)
-  }
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
 
   return (
     <Router>
       <div className='grid-container'>
-        <Header OpenSidebar={OpenSidebar}/>
-        <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product />} />
-        </Routes>
+        {isAuthenticated ? (
+          <>
+            <Header OpenSidebar={OpenSidebar} />
+            <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar} />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product" element={<Product />} />
+            </Routes>
+          </>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<Login setAuth={setIsAuthenticated} />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        )}
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
